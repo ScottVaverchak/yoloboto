963873f4126b68af869fe1377d75f60bab256738
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2021-02-28 19:27:43 -0500

    Yahoooooooooooooooooooooooooooooooooooooo. Finance.

--- main.js
+++ main.js
@@ -21,6 +21,8 @@ const escapeHTML = str => str.replace(/[&<>'"]/g,
 
 const asMoney = n => n.toLocaleString('en-US', { style: 'currency', currency: 'USD' })
 
+const toTheMoon = n => n > 25 ? '🚀🌑' : ''
+
 const main = async () => {
     const fileData = await (await fs.readFile('secret', 'utf8')).trim()
     keys = fileData.split('\n').reduce((prev, curr) => {
@@ -36,16 +38,16 @@ const main = async () => {
     // @TODO(svavs): There is a pattern forming between commands and how messages are invoked
     client.on('message', async message => {
 
-        if(message.content.includes('$$') && message.content.substr(0, 2) !== '$$') {
+        if (message.content.includes('$$') && message.content.substr(0, 2) !== '$$') {
             const [j1, symsec] = message.content.split('$$')
             const [sym, j2] = symsec.split(' ')
             const { ok, reply } = await getStock(sym)
 
-            if(ok) {
+            if (ok) {
                 message.channel.send(reply)
             }
 
-            return    
+            return
         }
 
         if (message.content.substr(0, 2) !== '$$') return
@@ -62,9 +64,9 @@ const main = async () => {
             message.channel.send(`List of commands: \n${cons}`)
         } else {
             const [sym, j3] = uc.split(' ')
-            const {ok, reply} = await getStock(sym)
+            const { ok, reply } = await getStock(sym)
 
-            message.channel.send(reply)
+            if(reply !== undefined) message.channel.send(reply)
         }
     });
 
@@ -73,19 +75,57 @@ const main = async () => {
 
 const getStock = async (sym) => {
     try {
-        const avurl = `https://cloud.iexapis.com/stable/stock/${escapeHTML(sym)}/quote?token=${keys['IEX']}`
-        const data = await fetch(avurl)
+        const yfurl = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${sym}`
+        const data = await fetch(yfurl)
+
         if (!data.ok) {
             return { ok: false, reply: `Unable to locate **${sym}**` }
         }
 
-        const stock = await data.json()
-        if (stock.isUSMarketOpen) {
-            return { ok: true, reply: `**${sym.toUpperCase()}**: ${asMoney(stock.latestPrice)} (${suffix}${asMoney(stock.change)}) ${moji}` }
-        } else {
-            const [moji, suffix] = stock.change > 0 ? ['📈', '+'] : ['📉', '']
-            return { ok: true, reply: `Premarket data is being worked on...\n**${sym.toUpperCase()}**: ${asMoney(stock.latestPrice)} (${suffix}${asMoney(stock.change)}) ${moji}` }
+        const stocks = await data.json()
+        console.log(stocks)
+        if (stocks.quoteResponse.result === undefined || stocks.quoteResponse.result.length === 0) {
+            return { ok: false, reply: `Unable to locate **${sym}**` }
         }
+
+        const [stock] = stocks.quoteResponse.result
+        console.log(stock)
+
+        if (stock.quoteType !== 'EQUITY') {
+            return { ok: false, reply: `I don't know what a ${stock.quoteType.toLowerCase()} is...` }
+        }
+
+        // @TODO(sjv): Yahoos finance API is strange - we should move all this logic to another function / file 
+        //             The fn should return the data we need in a std format
+        const ms = stock.marketState.replace('POSTPOST', 'POST').replace('CLOSED', 'POST')
+
+        if (ms === "PRE" || ms === "POST") {
+            let afterMarketMessage = undefined
+            try {
+                const state = ms.toLowerCase()
+                const marketChange = stock[`${state}MarketChange`]
+                const price = asMoney(stock[`${state}MarketPrice`])
+                const change = asMoney(stock[`${state}MarketChange`])
+                const percent = stock[`${state}MarketChangePercent`].toFixed(2)
+
+                const [premoji, presuffix] = marketChange > 0 ? ['📈', '+'] : ['📉', '']
+                afterMarketMessage = `**After Hours: ${sym.toUpperCase()}**: ${price} (${presuffix}${change}) (${percent}%) ${premoji} ${toTheMoon(stock[`${state}MarketChangePercent`])}`;
+            } catch (error) { }
+
+
+            const [moji, suffix] = stock.regularMarketChange > 0 ? ['📈', '+'] : ['📉', '']
+
+            const dailyMarket = `**At Close: ${sym.toUpperCase()}**: ${asMoney(stock.regularMarketPrice)} (${suffix}${asMoney(stock.regularMarketChange)}) (${stock.regularMarketChangePercent.toFixed(2)}%) ${moji} ${toTheMoon(stock.regularMarketChangePercent)}`
+            const msg = afterMarketMessage === undefined ? dailyMarket : [afterMarketMessage, dailyMarket].join('\n')
+
+            return { ok: true, reply:  msg }
+        } else if(ms === 'REGULAR') {
+            const [moji, suffix] = stock.regularMarketChange > 0 ? ['📈', '+'] : ['📉', '']
+            return { ok: true, reply: `**${sym.toUpperCase()}**: ${asMoney(stock.regularMarketPrice)} (${suffix}${asMoney(stock.regularMarketChange)}) (${stock.regularMarketChangePercent.toFixed(2)}%) ${moji} ${toTheMoon(stock.regularMarketChangePercent)}` }
+        }
+
+        return { ok: false, reply: undefined }
+
     } catch (error) {
         console.log(error)
         return { ok: false, reply: 'Oh no, I pooped myself 😞' }
